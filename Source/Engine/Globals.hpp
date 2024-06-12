065f2fa0c308306a9a6ccad66d87357340c542eb
Simo264 <simonevaiasinni@gmail.com>
2024-06-12 10:52:31 +0200

    Global variables

---
+++ Source/Engine/Globals.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+extern int WINDOW_WIDTH;
+extern int WINDOW_HEIGHT;
+extern float GAMMA_CORRECTION;
+
+extern float Z_NEAR;
+extern float Z_FAR;
+extern float LEFT;
+extern float RIGHT;
+extern float BOTTOM;
+extern float TOP;
